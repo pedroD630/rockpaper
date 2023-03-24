d58c7f07e950869acf11c578400bdeeab2735a44
Pedro Silva <pedrildojw@gmail.com>
2023-03-24 10:51:12 -0300

    Melhorar função de escolha do computador e adicionar comparação index-escolha à função

--- script.js
+++ script.js
@@ -2,53 +2,54 @@
 function getComputerChoice(){
     let possibleChoices = ["Rock", "Paper", "Scissor"];
     let choice = Math.floor(Math.random() * possibleChoices.length);
-    return choice;
+    let computerChoice;
+    switch (choice) {
+        case 0 :
+            computerChoice = "rock";
+            break;
+        case 1 :
+            computerChoice = "paper";
+            break;
+        case 2 : 
+            computerChoice = "scissor";
+            break;
+    }
+    return computerChoice;
 }
 let computerChoice = getComputerChoice();
-let choice = "";
-switch (computerChoice) {
-    case 0 :
-        choice = "rock";
-        break;
-    case 1 :
-        choice = "paper";
-        break;
-    case 2 : 
-        choice = "scissor";
-        break;
-}
+
 function compareValues(){
     
 }
 //receber escolha do usuario
 let userChoice = prompt("Rock, Paper or Scissor?").toLowerCase();
 
-if(choice == "rock" && userChoice == "rock"){
-    console.log("Draw. You choose " + userChoice + ". Computer choose " + choice);
+if(computerChoice == "rock" && userChoice == "rock"){
+    console.log("Draw. You choose " + userChoice + ". Computer choose " + computerChoice);
 }
-else if(choice == "rock" && userChoice == "paper"){
-    console.log("You Win! Congratulations! You choose " + userChoice + ". Computer choose " + choice);
+else if(computerChoice == "rock" && userChoice == "paper"){
+    console.log("You Win! Congratulations! You choose " + userChoice + ". Computer choose " + computerChoice);
 }
-else if(choice == "rock" && userChoice == "scissor"){
-    console.log("Nooooo! You was defeated! Try Again! You choose " + userChoice + ". Computer choose " + choice);
+else if(computerChoice == "rock" && userChoice == "scissor"){
+    console.log("Nooooo! You was defeated! Try Again! You choose " + userChoice + ". Computer choose " + computerChoice);
 }
-else if(choice == "paper" && userChoice == "rock"){
-    console.log("Nooooo! You was defeated! Try Again! You choose " + userChoice + ". Computer choose " + choice);
+else if(computerChoice == "paper" && userChoice == "rock"){
+    console.log("Nooooo! You was defeated! Try Again! You choose " + userChoice + ". Computer choose " + computerChoice);
 }
-else if(choice == "paper" && userChoice == "paper"){
-    console.log("Draw! You choose " + userChoice + ". Computer choose " + choice);
+else if(computerChoice == "paper" && userChoice == "paper"){
+    console.log("Draw! You choose " + userChoice + ". Computer choose " + computerChoice);
 }
-else if(choice == "paper" && userChoice == "scissor"){
-    console.log("You Win! Congratulations! You choose " + userChoice + ". Computer choose " + choice);
+else if(computerChoice == "paper" && userChoice == "scissor"){
+    console.log("You Win! Congratulations! You choose " + userChoice + ". Computer choose " + computerChoice);
 }
-else if(choice == "scissor" && userChoice == "rock"){
-    console.log("You Win! Congratulations! You choose " + userChoice + ". Computer choose " + choice);
+else if(computerChoice == "scissor" && userChoice == "rock"){
+    console.log("You Win! Congratulations! You choose " + userChoice + ". Computer choose " + computerChoice);
 }
-else if(choice == "scissor" && userChoice == "paper"){
-    console.log("Nooooo! You was defeated! Try Again! You choose " + userChoice + ". Computer choose " + choice);
+else if(computerChoice == "scissor" && userChoice == "paper"){
+    console.log("Nooooo! You was defeated! Try Again! You choose " + userChoice + ". Computer choose " + computerChoice);
 }
-else if(choice == "scissor" && userChoice == "scissor"){
-    console.log("Draw You choose " + userChoice + ". Computer choose " + choice);
+else if(computerChoice == "scissor" && userChoice == "scissor"){
+    console.log("Draw You choose " + userChoice + ". Computer choose " + computerChoice);
 }
 else{
     console.log("Invalid choice. You choose " + userChoice);
