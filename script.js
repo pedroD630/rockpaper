85c98b3f1ba9b8301b40d44f3ace5cb508eb3c97
Pedro Silva <pedrildojw@gmail.com>
2023-03-24 11:14:07 -0300

    Separar função para comparação das escolhas
    Criar função especifica para as escolhas e melhorar legibilidade de código. Diminuir numero de linhas e condicionais.

--- script.js
+++ script.js
@@ -16,43 +16,31 @@ function getComputerChoice(){
     }
     return computerChoice;
 }
-let computerChoice = getComputerChoice();
 
-function compareValues(){
-    
+
+function compareValues(computerChoice, userChoice){
+    if(computerChoice == "rock" && userChoice == "rock" || computerChoice == "paper" && userChoice == "paper" || computerChoice == "scissor" && userChoice == "scissor"){
+        console.log(`Draw. You choose ${userChoice} Computer choose ${computerChoice}`);
+    }
+    else if(computerChoice == "rock" && userChoice == "paper" || computerChoice == "paper" && userChoice == "scissor" || computerChoice == "scissor" && userChoice == "rock"){
+        console.log(`You Win! Congratulations! You choose ${userChoice} Computer choose ${computerChoice}`);
+    }
+    else if(computerChoice == "rock" && userChoice == "scissor" || computerChoice == "paper" && userChoice == "rock" || computerChoice == "scissor" && userChoice == "paper"){
+        console.log(`Nooooo! You was defeated! Try Again! You choose ${userChoice} Computer choose ${computerChoice}`);
+    }
+    else{
+        console.log("Invalid choice. You choose " + userChoice);
+    }
 }
 //receber escolha do usuario
 let userChoice = prompt("Rock, Paper or Scissor?").toLowerCase();
+let computerChoice = getComputerChoice();
 
-if(computerChoice == "rock" && userChoice == "rock"){
-    console.log("Draw. You choose " + userChoice + ". Computer choose " + computerChoice);
-}
-else if(computerChoice == "rock" && userChoice == "paper"){
-    console.log("You Win! Congratulations! You choose " + userChoice + ". Computer choose " + computerChoice);
-}
-else if(computerChoice == "rock" && userChoice == "scissor"){
-    console.log("Nooooo! You was defeated! Try Again! You choose " + userChoice + ". Computer choose " + computerChoice);
-}
-else if(computerChoice == "paper" && userChoice == "rock"){
-    console.log("Nooooo! You was defeated! Try Again! You choose " + userChoice + ". Computer choose " + computerChoice);
-}
-else if(computerChoice == "paper" && userChoice == "paper"){
-    console.log("Draw! You choose " + userChoice + ". Computer choose " + computerChoice);
-}
-else if(computerChoice == "paper" && userChoice == "scissor"){
-    console.log("You Win! Congratulations! You choose " + userChoice + ". Computer choose " + computerChoice);
-}
-else if(computerChoice == "scissor" && userChoice == "rock"){
-    console.log("You Win! Congratulations! You choose " + userChoice + ". Computer choose " + computerChoice);
-}
-else if(computerChoice == "scissor" && userChoice == "paper"){
-    console.log("Nooooo! You was defeated! Try Again! You choose " + userChoice + ". Computer choose " + computerChoice);
-}
-else if(computerChoice == "scissor" && userChoice == "scissor"){
-    console.log("Draw You choose " + userChoice + ". Computer choose " + computerChoice);
-}
-else{
-    console.log("Invalid choice. You choose " + userChoice);
-}
+gameResult = compareValues(computerChoice, userChoice);
+
+
+
+
+
 
 
